adb7edac501ae8898a2ec24b7ec06ccc003cf286
Dharanish A M <dharanish816@gmail.com>
2025-08-01 21:21:56 +0530

    Model - Entity Codes

--- demo/src/main/java/com/example/demo/model/Student.java
+++ demo/src/main/java/com/example/demo/model/Student.java
@@ -1,5 +1,11 @@
 package com.example.demo.model;
 
+import jakarta.persistence.*;
+@Entity
 public class Student {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int age;
 }
